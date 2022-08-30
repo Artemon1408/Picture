4859f0c7381d2cc78ab56dfdc80c747f8afe85d6
Artem Drozhzhyn <12Artemondrozhzhyn@gmail.com>
2022-08-30 18:52:25 +0300

    first commit

---
+++ src/js/modules/mask.js
@@ -0,0 +1,51 @@
+const mask = (selector) => {
+    function creatMask(event) {
+
+        let setCursorPosition = (pos, elem) => {
+            elem.focus();
+
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                let range = elem.createTextRange();
+
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
+
+        let matrix = '+3 (___) ___ __ __',
+            i = 0,
+            def = matrix.replace(/\D/g, ''),
+            val = this.value.replace(/\D/g, '');
+
+        if (def.length >= val.length) {
+            val = def;
+        }
+
+        this.value = matrix.replace(/./g, function(a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+        });
+
+        if (event.type === 'blur') {
+            if (this.value.length == 2) {
+                this.value = '';
+            }
+        } else {
+            setCursorPosition(this.value.length, this);
+        }
+    }
+
+    let inputs = document.querySelectorAll(selector);
+
+    inputs.forEach(input  => {
+        input.addEventListener('input', creatMask);
+        input.addEventListener('focus', creatMask);
+        input.addEventListener('blur', creatMask);
+    });
+
+};
+
+export default mask;
